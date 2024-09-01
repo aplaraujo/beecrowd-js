66b7feecc2ab68fdc2eb907b8fa35aba40c85514
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-01 16:14:04 -0300

    1017

---
+++ src/1017.js
@@ -0,0 +1,21 @@
+const { get } = require('http');
+
+const input = require('fs').readFileSync('stdin', 'utf8');
+const lines = input.split('\n');
+
+const getFuelQuantity = (veloc, time) => {
+    const dist = veloc * time;
+    const fuel = dist / 12;
+    return fuel.toFixed(3);
+}
+
+const numArr = [];
+
+for(let i = 0; i < lines.length; i++) {
+    numArr.push(parseInt(lines[i]));
+}
+
+const T = numArr[0];
+const V = numArr[1];
+
+console.log(getFuelQuantity(V, T));
