3da1fd9fca813c312b8ee50f7d3a8ed58872d93c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-01 15:22:12 -0300

    1004

---
+++ src/1004.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('stdin', 'utf8');
+const lines = input.split('\n');
+
+const numArr = [];
+
+for(let i = 0; i < lines.length; i++) {
+    numArr.push(parseInt(lines[i]));
+}
+
+const PROD = numArr[0] * numArr[1];
+
+console.log(`PROD = ${PROD}`);
