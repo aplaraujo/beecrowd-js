a60ea62dd1b61f8dfb24143cf0cf6d45a770ca1d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-01 15:19:23 -0300

    Exemplo

--- src/exemplo.js
+++ src/exemplo.js
@@ -1,2 +1,8 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
+
+const numArr = [];
+
+for(let i = 0; i < lines.length; i++) {
+    numArr.push(parseInt(lines[i]));
+}
