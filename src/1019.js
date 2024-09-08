00802220f738027a6381af8225c2606cdff2bcdb
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-08 14:42:53 -0300

    1019

--- src/1019.js
+++ src/1019.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-getTime = (val) => {
+const getTime = (val) => {
     const hour = Math.floor(val / 3600);
     const minutes = Math.floor((val - (hour * 3600)) / 60);
     const seconds = val % 60;
