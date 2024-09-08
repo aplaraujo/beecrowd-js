217c2e9282d4a54a45367e44ff787be67195cfb5
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-08 09:50:07 -0300

    1153

---
+++ src/1153.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('stdin', 'utf8');
+const lines = input.split('\n');
+
+const num = Number(lines);
+
+let fat = 1;
+
+for (let i = 1; i <= num; i++) {
+    fat = fat * i;
+}
+
+console.log(fat);
