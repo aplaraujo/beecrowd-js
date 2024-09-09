9ea47c79cb1e3e53c6507d8e885b0bd35699e6d7
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-09 18:27:20 -0300

    1113

--- src/1113.js
+++ src/1113.js
@@ -1,17 +1,18 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const getNumberOrder = (a, b) => {
-    return (a > b) ? "Crescente" : "Decrescente";
-}
+let x;
+let y;
 
-let arr = [];
-let str = '';
+do {
+    let line = lines.shift().split(' ');
+    x = Number(line[0]);
+    y = Number(line[1]);
 
-while (str = lines.shift()) {
-    arr = str.split(' ');
-    let X = Number(arr[0]);
-    let Y = Number(arr[1]);
-
-    console.log(getNumberOrder(X, Y));
-}
+    if (x < y) {
+        console.log('Crescente');
+    } else if (x > y) {
+        console.log('Decrescente');
+    }
+    
+} while (x !== y);
