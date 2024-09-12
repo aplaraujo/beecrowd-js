6eeaabbc66c84e2f9facc8b6018147e3883e785d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-12 18:40:55 -0300

    1021

--- src/1021.js
+++ src/1021.js
@@ -7,15 +7,15 @@ const notas = [100, 50, 20, 10, 5, 2];
 const moedas = [1.0, 0.5, 0.25, 0.1, 0.05, 0.01];
 
 console.log("NOTAS:");
-for (let nota of notas) {
-  let saque = parseInt(val / nota);
-  console.log(`${saque} nota(s) de R$ ${nota}.00`);
-  val = val % nota;
+for(let i = 0; i < notas.length; i++) {
+    let saque = parseInt(val / notas[i]);
+    console.log(`${saque} nota(s) de R$ ${notas[i]}.00`);
+    val = val % notas[i];
 }
 
 console.log("MOEDAS:");
-for (let moeda of moedas) {
-  saque = parseInt(val / moeda);
-  console.log(`${saque} moeda(s) de R$ ${moeda.toFixed(2)}`);
-  val = val % moeda + 0.00001;
+for(let i = 0; i < moedas.length; i++) {
+    let saque = parseInt(val / moedas[i]);
+    console.log(`${saque} moeda(s) de R$ ${moedas[i].toFixed(2)}`);
+    val = val % moedas[i] + 0.00001;
 }
