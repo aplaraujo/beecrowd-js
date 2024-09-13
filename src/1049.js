89aea6a6f9119f7afad690f0ab5e1095adb6f4ea
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-12 22:36:58 -0300

    1049

---
+++ src/1049.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync('stdin', 'utf8').split('\n');
+
+let tipo1 = input[0];
+let tipo2 = input[1];
+let tipo3 = input[2];
+ 
+if (tipo1 === "vertebrado" && tipo2 === "ave" && tipo3 === "carnivoro") {
+		console.log("aguia");
+} else if (tipo1 === "vertebrado" && tipo2 === "ave" && tipo3 === "onivoro") {
+		console.log("pomba");
+} else if (tipo1 === "vertebrado" && tipo2 === "mamifero" && tipo3 === "onivoro") {
+		console.log("homem");
+} else if (tipo1 === "vertebrado" && tipo2 === "mamifero" && tipo3 === "herbivoro") {
+		console.log("vaca");
+} else if (tipo1 === "invertebrado" && tipo2 === "inseto" && tipo3 === "hematofago") {
+		console.log("pulga");
+} else if (tipo1 === "invertebrado" && tipo2 === "inseto" && tipo3 === "herbivoro") {
+		console.log("lagarta");
+} else if (tipo1 === "invertebrado" && tipo2 === "anelideo" && tipo3 === "hematofago") {
+		console.log("sanguessuga");
+} else if (tipo1 === "invertebrado" && tipo2 === "anelideo" && tipo3 === "onivoro") {
+		console.log("minhoca");
+}
