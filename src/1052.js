2f13f2e73e648de685f66c1633488948d3cea3c8
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-13 18:41:38 -0300

    1052

---
+++ src/1052.js
@@ -0,0 +1,30 @@
+const input = require('fs').readFileSync('stdin', 'utf8');
+const lines = input.split('\n');
+
+let month = Number(lines.shift());
+
+if (month === 1) {
+	console.log("January");
+} else if (month === 2) {
+	console.log("February");
+} else if (month === 3) {
+	console.log("March");
+} else if (month === 4) {
+	console.log("April");
+} else if (month === 5) {
+	console.log("May");
+} else if (month === 6) {
+	console.log("June");
+} else if (month === 7) {
+	console.log("July");
+} else if (month === 8) {
+	console.log("August");
+} else if (month === 9) {
+	console.log("September")
+} else if (month === 10) {
+	console.log("October")
+} else if (month === 11) {
+	console.log("November")
+} else if (month === 12) {
+	console.log("December")
+} 
