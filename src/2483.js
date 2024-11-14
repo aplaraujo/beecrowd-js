a55be7db52739507e7a6051893e13649c694bc1a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-14 18:13:04 -0300

    2483

---
+++ src/2483.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('stdin', 'utf8');
+const lines = input.split('\n');
+
+const I = Number(lines[0]);
+
+let str = "";
+
+for (let i = 1; i <= I; i++) {
+    str += "a" 
+}
+
+console.log(`Feliz nat${str}l!`);
