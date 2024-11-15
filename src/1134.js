38178e4caf4eb8b2acfa2c5b568420dba081a7d9
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-15 11:53:44 -0300

    1134

--- src/1134.js
+++ src/1134.js
@@ -1,31 +1,22 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const numArr = lines.map(num => Number(num));
+const arr = lines.map(num => Number(num));
 
-let op;
 let alcool = 0;
 let gasolina = 0;
 let diesel = 0;
 
-for (let i = 0; i < numArr.length; i++) {
-    op = numArr[i];
-
-    while (op !== 4) {
-        if (op === 1) {
-            alcool++;
-        }
-        if (op === 2) {
-            gasolina++;
-        }
-        if (op === 3) {
-            diesel++;
-        }
-        i++
-
-        op = numArr[i];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+        alcool++;
+    } else if (arr[i] === 2) {
+        gasolina++;
+    } else if (arr[i] === 3) {
+        diesel++;
+    } else if (arr[i] === 4) {
+        break;
     }
-
 }
 
 
