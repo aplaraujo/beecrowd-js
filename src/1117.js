bb6af195d963f6ad9d1ab50299ceceba3331c937
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-15 11:16:22 -0300

    1117

--- src/1117.js
+++ src/1117.js
@@ -1,18 +1,20 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const numArr = lines.map(num => Number(num));
-
+const arr = lines.map(num => Number(num));
 const validGrades = [];
 
-for (let i = 0; i < numArr.length; i++) {
-    if (numArr[i] >= 0 && numArr[i] <= 10) {
-        validGrades.push(numArr[i]);
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] < 0 || arr[i] > 10) {
+        console.log("nota invalida");
     } else {
-       console.log("nota invalida") 
+        validGrades.push(arr[i]);
     }
 }
 
-const average = validGrades.reduce((acc, grade) => acc + grade, 0) / 2;
+const grade1 = validGrades[0];
+const grade2 = validGrades[1];
+
+const average = (grade1 + grade2) / 2;
 
 console.log(`media = ${average.toFixed(2)}`);
