f0287ef5fa16687cc3208712a55c1891100b2774
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-15 14:20:23 -0300

    1142

--- src/1142.js
+++ src/1142.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const N = Number(lines);
+const N = Number(lines.shift());
 
 for (let i = 0; i < N; i++) {
     console.log(`${4 * i + 1} ${4 * i + 2} ${4 * i + 3} PUM`);
