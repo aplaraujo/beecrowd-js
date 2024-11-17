0661495d77b70f19668060a4ec3a3ff833c2fcf2
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-17 09:27:11 -0300

    1143

--- src/1143.js
+++ src/1143.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const N = Number(lines);
+const N = Number(lines[0]);
 
 for (let i = 0; i < N; i++) {
     console.log(`${i + 1} ${Math.pow((i + 1), 2)} ${Math.pow((i + 1), 3)}`);
