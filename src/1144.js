cb31a8788b130e191e71031ce935d75d8038e0b7
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-17 09:27:29 -0300

    1144

--- src/1144.js
+++ src/1144.js
@@ -1,8 +1,9 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const N = Number(lines);
+const N = Number(lines[0]);
 
-for (let i = 1; i <= N; i++) {
-    console.log(`${i} ${i*i} ${i*i*i}\n${i} ${i*i*i} ${i*i*i*i}`);
+for (let i = 0+1; i < N; i++) {
+    console.log(`${i} ${i*i} ${i*i*i}`);
+    console.log(`${i} ${(i*i)+1} ${(i*i*i)+1}`);
 }
