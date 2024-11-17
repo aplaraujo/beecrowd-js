7c3eeea29722f84be371af96d1106220cb0c4264
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-17 11:47:20 -0300

    1177

--- src/1177.js
+++ src/1177.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('stdin', 'utf8');
 const lines = input.split('\n');
 
-const N = Number(lines);
+const N = Number(lines[0]);
 
 for (let i = 0; i < 1000; i++) {
     console.log(`N[${i}] = ${i % N}`);
