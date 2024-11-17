1b02c52400fdf40979df2501f6c3cf286b92b29b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-11-17 11:26:25 -0300

    1172

--- src/1172.js
+++ src/1172.js
@@ -5,10 +5,8 @@ const arr = lines.map(num => Number(num));
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] <= 0) {
-        arr[i] = 1;
+        console.log(`X[${i}] = ${1}`);
+    } else {
+        console.log(`X[${i}] = ${arr[i]}`);
     }
-}
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(`X[${i}] = ${arr[i]}`);
-}
+};
